9ec979425f7b14da99ba8610f59e7d2f4b7888cf
Michael Wekesa <wekesa360@outlook.com>
2024-04-20 11:29:19 +0300

    Update dependencies in conda.yaml and shell command in robot.yaml

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from src.news_extractor_process import NewsExtractorProcess
 from robocorp.tasks import task
 
-@task
+
 def news_extractor_process_task():
     news_extractor = NewsExtractorProcess()
     news_extractor.run_news_extractor()
+
+if __name__ == "__main__":
+    news_extractor_process_task()
