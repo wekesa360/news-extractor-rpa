d1cffafd51f2821e9a9f27c8266eed98efc86aad
Michael Wekesa <wekesa360@outlook.com>
2024-04-22 19:13:40 +0300

    fix bug: change, images path

--- src/news_extractor_process.py
+++ src/news_extractor_process.py
@@ -18,7 +18,7 @@ class NewsExtractorProcess:
         self.logging_manager = LoggingManager("output/news_extractor.log")
         self.browser_automator = BrowserAutomator(self.logging_manager)
         self.config_manager = ConfigManager("./config.yaml", self.logging_manager)
-        self.image_downloader = ImageDownloader("output/images", self.logging_manager)
+        self.image_downloader = ImageDownloader("output/", self.logging_manager)
         self.excel_exporter = ExcelExporter(
             "output/extracted_articles.xlsx", self.logging_manager
         )
